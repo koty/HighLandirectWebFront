287a612d708912c6f83953e3b6737b553f4f153c
koty <kouichi.nishizawa+github@gmail.com>
2017-08-15 10:57:33 +0900

    1st commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import CustomerList from '@/components/CustomerList'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/customers/',
+      name: 'CusrtomerList',
+      component: CustomerList
+    }
+  ]
+})
