4e45f8d8d04700cd98214fc3ca745f5a2149409a
koty <kouichi.nishizawa+github@gmail.com>
2017-09-27 18:57:58 +0900

    splitterをつけてみた

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -2,20 +2,29 @@
   <div>
     <h2>注文入力</h2>
     <md-button class="md-raised" v-on:click="">新規注文入力</md-button>
-    <div>
-      <span>注文番号</span><span>{{ order_no }}</span>
-      <span>送り主顧客番号</span><span>{{ send_customer_id }}</span>
-    </div>
-    <md-button class="md-raised" v-on:click="">宛先追加</md-button>
-    <md-button class="md-raised" v-on:click="delete_order">選択した宛先を消す</md-button>
-    <li v-for="detail in details">
-      <input type="checkbox" v-model="detail.selected" :id="'ordercheck' + detail.receive_customer_id"/>
-      <label :for="'ordercheck' + detail.receive_customer_id">{{ detail.product_id }}:{{ detail.receive_customer_id }}</label>
-    </li>
+    <splitter :margin="20">
+      <div slot="left-pane">
+        <div>
+          <span>注文番号</span><span>{{ order_no }}</span>
+          <span>送り主顧客番号</span><span>{{ send_customer_id }}</span>
+        </div>
+        <md-button class="md-raised" v-on:click="">宛先追加</md-button>
+        <md-button class="md-raised" v-on:click="delete_order">選択した宛先を消す</md-button>
+        <li v-for="detail in details">
+          <input type="checkbox" v-model="detail.selected" :id="'ordercheck' + detail.receive_customer_id"/>
+          <label :for="'ordercheck' + detail.receive_customer_id">{{ detail.product_id }}:{{ detail.receive_customer_id }}</label>
+        </li>
+      </div>
+      <div slot="right-pane">
+        <customer-list></customer-list>
+      </div>
+    </splitter>
   </div>
 </template>
 
 <script>
+  import CustomerList from '@/components/CustomerList'
+  import VueSplitter from '@rmp135/vue-splitter'
   export default {
     created: function () {
     },
@@ -32,7 +41,7 @@
         this.$store.commit('delete_selected_order', {order_no: this.$route.params.order_no})
       }
     },
-    components: {}
+    components: {'customer-list': CustomerList, 'splitter': VueSplitter}
   }
 </script>
 
