064d14c85593d96f843210c0f717a35b9b4c103c
koty <kouichi.nishizawa+github@gmail.com>
2017-09-27 18:14:48 +0900

    ラベルを追加。

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <h2>注文入力</h2>
+    <md-button class="md-raised" v-on:click="">新規注文入力</md-button>
     <div>
       <span>注文番号</span><span>{{ order_no }}</span>
       <span>送り主顧客番号</span><span>{{ send_customer_id }}</span>
     </div>
-    <md-button class="md-raised" v-on:click="">注文追加</md-button>
-    <md-button class="md-raised" v-on:click="delete_order">選択した注文を消す</md-button>
+    <md-button class="md-raised" v-on:click="">宛先追加</md-button>
+    <md-button class="md-raised" v-on:click="delete_order">選択した宛先を消す</md-button>
     <li v-for="detail in details">
-      <input type="checkbox" v-model="detail.selected"/>{{ detail.product_id }}:{{ detail.receive_customer_id }}
+      <input type="checkbox" v-model="detail.selected" :id="'ordercheck' + detail.receive_customer_id"/>
+      <label :for="'ordercheck' + detail.receive_customer_id">{{ detail.product_id }}:{{ detail.receive_customer_id }}</label>
     </li>
   </div>
 </template>
